a254e0b8a387529e85e218a80811392d74e68b08
victorolayemi <testimonyismine@gmail.com>
2023-01-21 15:32:37 +0100

    nnnn

---
+++ precache-manifest.bbc5a590e0bc33da3cd8b0589c0cc90b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e6fb50a501cc7d2a6b9b1fe2f3d1fc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01c6a516127a6fd33e28",
+    "url": "/static/css/2.6c0eed84.chunk.css"
+  },
+  {
+    "revision": "617d2c4a96488f7fd7db",
+    "url": "/static/css/main.e1c54cc8.chunk.css"
+  },
+  {
+    "revision": "01c6a516127a6fd33e28",
+    "url": "/static/js/2.cb5f290d.chunk.js"
+  },
+  {
+    "revision": "aab6d6489f375857eb631f4b78d891f0",
+    "url": "/static/js/2.cb5f290d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "617d2c4a96488f7fd7db",
+    "url": "/static/js/main.4059b0c7.chunk.js"
+  },
+  {
+    "revision": "a6eea984af5e3f6da016",
+    "url": "/static/js/runtime~main.23199889.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
